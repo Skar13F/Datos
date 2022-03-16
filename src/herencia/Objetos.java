82a33215deadca7ca0e561467f0a21bc088de86a
Skar13F <of5162@gmail.com>
2022-03-16 15:27:14 -0600

    commit 1, datos, objetos y clases

---
+++ src/herencia/Objetos.java
@@ -0,0 +1,14 @@
+package herencia;
+
+public class Objetos extends Datos {
+	private String Objeto;
+
+	public String getObjeto() {
+		return Objeto;
+	}
+
+	public void setObjeto(String objeto) {
+		Objeto = objeto;
+	}
+
+}
---
+++ src/herencia/PNumerico.java
@@ -0,0 +1,9 @@
+package herencia;
+
+public class PNumerico extends Primitivos{
+	PNumerico() {
+		setNombre("byte");
+		System.out.println("El nombre es: " + getNombre());
+	}
+
+}
